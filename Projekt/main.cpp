856f7baf3bae8c538a0242163d93b0e25e243872
Daniel Wilk <daniel.3222a@gmail.com>
2024-01-06 13:07:06 +0100

    [projekt] Main: New reading function

--- Projekt/main.cpp
+++ Projekt/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <string>
 
+#include "./console.h"
+#include "./color.h"
+
+void loop(Console::Console& cons) {
+    Color::print(Color::WHITE, Color::NONE, Color::BOLD, "Console::Console");
+    std::cout << " => ";
+    std::cout << cons.curr_line;
+    std::cout << std::endl;
+}
+
 int main() {
+    Console::Console cons = Console::console_init_dev();
+    Console::console_start_reading(cons, &loop);
+
     return 0;
 }
